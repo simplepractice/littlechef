cff1d1e179de989b64da0fb4a028245c5779b9db
Javad Mustafayev <javad@simplepractice.com>
2019-07-08 11:12:02 -0700

    [#165698548] Let Gsheet to put time for us

--- littlechef/chef.py
+++ littlechef/chef.py
@@ -118,7 +118,7 @@ def record_chef_run(node, status):
     log_sheet = file.open("Chef Deployment Tracker").get_worksheet(1)
 
     hostname = node['name'].split('.')[0]
-    row_data = [hostname, branch, user, datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M"), status]
+    row_data = [hostname, branch, user, "=NOW()", status]
     hostnames = sheet.col_values(1)
 
     log_sheet.insert_row(row_data)
